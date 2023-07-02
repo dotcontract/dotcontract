28b4e6304a4fdc15ba367fe34359d8e481b83873
suryadheeshjith <Surya.dheeshjith@gmail.com>
2023-07-02 23:11:23 +0530

    Prettier

--- javascript/packages/cli/commands.test.js
+++ javascript/packages/cli/commands.test.js
@@ -1,8 +1,10 @@
+import { expect, describe, it } from "@jest/globals";
 import shell from "shelljs";
 
 describe("CLI-Tests", () => {
-
-    it("should run cli tests script", async () => {
-        expect(shell.exec('./cli-tests', {cwd: '../../../tests', silent: true}).stderr).toEqual("");
-    });
+  it("should run cli tests script", async () => {
+    expect(
+      shell.exec("./cli-tests", { cwd: "../../../tests", silent: true }).stderr
+    ).toEqual("");
+  });
 });
