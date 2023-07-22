2e6eee0db4016797a5002018d5c131abc64863e9
Foy Savas <foy@sav.as>
2023-07-22 14:37:43 -0400

    better logging for cli test failures

--- javascript/packages/cli/commands.test.js
+++ javascript/packages/cli/commands.test.js
@@ -1,18 +1,25 @@
 import { expect, describe, it } from "@jest/globals";
 import shell from "shelljs";
 
+function runShellScriptTest(name) {
+  const out = shell.exec(`./${name}`, {
+    cwd: "../../../tests",
+    silent: true,
+  });
+  if (out.code != 0) {
+    console.error(
+      `Non-zero exit code: ${out.code}.\n\nSTDOUT:\n\n${out.stdout} \n\nSTDERR:\n\n${out.stderr}`
+    );
+  }
+  return out;
+}
+
 describe("CLI-Tests", () => {
   it("should run cli tests script", async () => {
-    const out_dir = shell.exec("./cli-tests-directory", {
-      cwd: "../../../tests",
-      silent: true,
-    });
+    const out_dir = runShellScriptTest('cli-tests-directory');
     expect(out_dir.code).toEqual(0);
 
-    const out_file = shell.exec("./cli-tests-file", {
-      cwd: "../../../tests",
-      silent: true,
-    });
+    const out_file = runShellScriptTest('cli-tests-file');
     expect(out_file.code).toEqual(0);
   });
 });
