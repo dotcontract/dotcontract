b301316e277fc3e8e8e9be7673f11fa706516869
suryadheeshjith <Surya.dheeshjith@gmail.com>
2023-08-10 20:17:32 -0400

    Added synthesizer package

---
+++ javascript/packages/synthesizer/src/Synthesizer.js
@@ -0,0 +1,12 @@
+// import SSH2Promise from "ssh2-promise";
+// import path from "path";
+// import fs from "fs";
+// import temp from "temp";
+// temp.track();
+
+
+const log = console.log;
+
+export default class Synthesizer {
+  
+}
---
+++ javascript/packages/synthesizer/src/index.js
@@ -0,0 +1,4 @@
+export { default as Synthesizer } from "./Synthesizer.js";
+
+import Synthesizer from "./Synthesizer.js";
+export default Synthesizer;
