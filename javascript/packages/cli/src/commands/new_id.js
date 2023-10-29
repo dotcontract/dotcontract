ea5deedb0b3f1c23b89c04b2545e3cd715c88d2e
Foy Savas <foy@sav.as>
2023-10-28 20:00:28 -0400

    generate access key files

--- javascript/packages/cli/src/commands/new_id.js
+++ javascript/packages/cli/src/commands/new_id.js
@@ -6,6 +6,8 @@ export const builder = {
     desc: "name",
     default: "default",
   },
+  overwrite: {
+  }
 };
 
 import os from "os";
@@ -15,13 +17,17 @@ import Key from "@dotcontract/utils/Key";
 const log = console.log;
 
 export async function handler(argv) {
-  const { name } = argv;
+  const { name, overwrite } = argv;
 
   const base_path = `${os.homedir()}/.dotcontract/${name}`;
   const path_to_keypair = `${base_path}/signing.keypair`;
-  if (fs.existsSync(path_to_keypair)) {
-    console.error(`ERROR: "${name}" identity already exists`);
-    return;
+  if (fs.existsSync(path_to_keypair) ) {
+    if (!overwrite) {
+      console.error(`ERROR: "${name}" identity already exists`);
+      return;
+    } else {
+      console.log(`WARNING: "${name}" identity already exists, overwriting`);
+    }
   }
   const key = await Key.generate();
 
@@ -29,13 +35,14 @@ export async function handler(argv) {
   await key.asJSONFile(`${base_path}/signing.keypair`);
 
   const access_key = await Key.generate();
+  fs.mkdirSync(`${base_path}/access`, { recursive: true });
   await fs.writeFileSync(
     `${base_path}/access/id_ed25519`,
     await access_key.asSSHPrivatePem()
   );
   await fs.writeFileSync(
     `${base_path}/access/id_ed25519.pub`,
-    await access_key.fromSSHDotPub()
+    await access_key.asSSHDotPub()
   );
 
   log(`crypto:/${await key.asPublicMultiaddress()}`);
